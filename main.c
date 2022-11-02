f814ced0d9290910b2d30bda6b2aef564c9a2bec
jiadexie <837578259@qq.com>
2022-11-03 05:35:10 +0800

    continue用法

--- main.c
+++ main.c
@@ -1,21 +1,38 @@
 //统计字符数，单词数，行数
 #include <stdio.h>
-#include <ctype.h>
-#include <stdbool.h>
-#define COVERAGE 350
 int main(void)
 {
-    int sq_feet;
-    int cans;
+    const float MIN=0.0f;
+    const float MAX=100.0f;
 
-    printf("Enter number of square feet to be painted:\n");
-    while (scanf("%d",&sq_feet)==1)
+    float score;
+    float total=0.0f;
+    int n=0;
+    float min=MAX;
+    float max=MIN;
+
+    printf("Enter the first score(q to quit): ");
+    while (scanf("%f,&score")==1)
     {
-        cans=sq_feet/COVERAGE;
-        cans+=(sq_feet%COVERAGE==0)?0:1;
-        printf("You need %d %s of paint.\n",cans,cans==1?"can":"cans");
-        printf("Enter next value(q to quit):\n");
+        if(score<MIN||score>MAX)
+        {
+            printf("%0.1f is an invalid value.Try again:",score);
+            continue;
+        }
+        printf("Accepting %0.1f:\n",score);
+        min=(score<min)?score:min;
+        max=(score>max)?score:max;
+        total+=score;
+        n++;
+        printf("Enter next score（q to quit): ");
     }
+    if(n>0)
+    {
+        printf("Average of %d score is %0.1f.\n",n,total/n);
+        printf("Low=%0.1f,high=%0.1f\n",min,max);
+    }
+    else printf("No valid scores were entered.\n");
+
     return 0;
 
 }
