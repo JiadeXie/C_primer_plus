1ef3c76b929115478f3bf2de06f9f6198afc1662
jiadexie <837578259@qq.com>
2022-11-02 23:19:21 +0800

    更改输出，空格不变

--- main.c
+++ main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
+#define SPACE ' '
 int main(void)
 {
-    const int FREEZING=0;
-    float temperature;
-    int cold_days=0;
-    int all_days=0;
-    printf("Enter the list of daily low temperature.\n");
-    printf("Use Celsius,and enter q to quit.\n");
-    while (scanf("%lf",&temperature)==1)
-    {
-        all_days++;
-        if(temperature<FREEZING) cold_days++;
-    }
-    if(all_days!=0)
-        printf("%d days total: %.1f%% were below freezing.\n",
-               all_days,100.0*(float)cold_days/all_days);
-    if(all_days==0) printf("No data entered!\n");
+    char ch;
 
+    ch=getchar();
+    while (ch!='\n')
+    {
+        if(ch==SPACE) putchar(ch);
+        else putchar(ch+1);
+        ch=getchar();
+    }
+    putchar(ch);//打印换行符
     return 0;
 }
