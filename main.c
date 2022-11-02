7a59c006b547a1d9ce6e077e4f60fa964ee10eaf
jiadexie <837578259@qq.com>
2022-11-03 03:48:36 +0800

    使用逻辑与运算符

--- main.c
+++ main.c
@@ -1,28 +1,15 @@
 #include <stdio.h>
+#define PERIOD '.'
 int main(void)
 {
-    unsigned long num;//待测试的数
-    unsigned long div;//可能的约数
-    int isPrime;//素数标记
+    char ch;
+    int charcount=0;
 
-    printf("Please enter an integer for analysis; ");
-    printf("Enter q to quit.\n");
-    while (scanf("%lu",&num)==1)
+    while ((ch=getchar())!=PERIOD)
     {
-        for (div=2,isPrime=1;(div*div)<=num;div++)
-        {
-            if(num%div==0)
-            {
-                if((div*div)!=num) printf("%lu is divisible by %lu and %lu.\n",num,div,num/div);
-                else printf("%lu is divisible by %lu.\n",num,div);
-                isPrime=0;
-            }
-        }
-        if(isPrime) printf("%lu is prime.\n",num);
-        printf("Please enter another integer for analysis; ");
-        printf("Enter q to quit.\n");
+        if(ch!='"'&&ch!='\'') charcount++;
     }
-    printf("Bye.\n");
+    printf("There are %d non-quote characters.\n",charcount);
 
     return 0;
 
