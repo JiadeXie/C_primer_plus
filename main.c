e8e65719421e900b74cd6de36d7a76bdd9dbd2da
jiadexie <837578259@qq.com>
2022-11-03 01:09:32 +0800

    判断一个数是否为素数

--- main.c
+++ main.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
-#define RATE1 0.13230
-#define RATE2 0.15040
-#define RATE3 0.30025
-#define RATE4 0.34025
-#define BREAK1 360.0
-#define BREAK2 468.0
-#define BREAK3 720.0
-#define BASE1 (RATE1*BREAK1)
-#define BASE2 (BASE1+(RATE2*(BREAK2-BREAK1)))
-#define BASE3 (BASE2+(RATE3*(BREAK3-BREAK2)))
 int main(void)
 {
-    double kwh;
-    double bill;
-    printf("Please enter the kwh used.\n");
-    scanf("%lf",&kwh);
-    if(kwh<=BREAK1) bill=RATE1*kwh;
-    else if (kwh<=BREAK2) bill=BASE1+(RATE2*(kwh-BREAK1));
-    else if (kwh<=BREAK3) bill=BASE2+(RATE3*(kwh-BREAK2));
-    else bill=BASE3+(RATE4*(kwh-BREAK3));
-    printf("The charge for %.1f kwh is $%1.2f.\n",kwh,bill);
+    unsigned long num;//待测试的数
+    unsigned long div;//可能的约数
+    int isPrime;//素数标记
+
+    printf("Please enter an integer for analysis; ");
+    printf("Enter q to quit.\n");
+    while (scanf("%lu",&num)==1)
+    {
+        for (div=2,isPrime=1;(div*div)<=num;div++)
+        {
+            if(num%div==0)
+            {
+                if((div*div)!=num) printf("%lu is divisible by %lu and %lu.\n",num,div,num/div);
+                else printf("%lu is divisible by %lu.\n",num,div);
+                isPrime=0;
+            }
+        }
+        if(isPrime) printf("%lu is prime.\n",num);
+        printf("Please enter another integer for analysis; ");
+        printf("Enter q to quit.\n");
+    }
+    printf("Bye.\n");
 
     return 0;
 
