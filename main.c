2814c4c48a29aaf35b8ef6a46594e9b2e4715ce9
jiadexie <837578259@qq.com>
2022-11-04 06:26:47 +0800

    计算工资.v2

--- main.c
+++ main.c
@@ -1,38 +1,81 @@
 #include <stdio.h>
-#define BASESAL 10.0
+#define SAL1 8.75
+#define SAL2 9.33
+#define SAL3 10.00
+#define SAL4 11.20
 #define BASETIME 40.0
-#define BASERATE1 0.15
-#define BASERATE2 0.20
-#define BASERATE3 0.25
-
+#define RATEBASE1 300.0
+#define RATEBASE2 450.0
+#define RATE1 0.15
+#define RATE2 0.20
+#define RATE3 0.25
 
 int main(void)
 {
-    float workTime,salary,tax;
-    printf("please enter work time in a week:\n");
-    scanf("%f",&workTime);
-    if(workTime<=BASETIME) salary=workTime*BASESAL;
-    if(workTime>BASETIME)
+    void showsal(double,double);
+    int flag;
+    double time;
+    printf("**********************************************************************\n");
+    printf("Enter the number corresponding to the desired pay rate or action:\n");
+    printf("1) $8.75/hr\t\t\t2) $9.33/hr\n");
+    printf("3) $10.00/hr\t\t\t4) $11.20/hr\n");
+    printf("5) quit\n");
+    printf("**********************************************************************\n");
+    scanf("%d",&flag);
+    while (!(flag==5))
     {
-        workTime=BASETIME+(workTime-BASETIME)*1.5;
-        salary=workTime*BASESAL;
-    }
-    if(salary<=300.0)
-    {
-        tax=salary*BASERATE1;
-        printf("salary=%.2f,tax=%.2f,sal after tax=%.2f",salary,tax,salary-tax);
-    }
-    if(salary>300.0&&salary<=450.0)
-    {
-        tax=300.0*BASERATE1+(salary-300.0)*BASERATE2;
-        printf("salary=%.2f,tax=%.2f,sal after tax=%.2f",salary,tax,salary-tax);
-    }
-    if(salary>450.0)
-    {
-        tax=300.0*BASERATE1+150*BASERATE2+(salary-450.0)*BASERATE3;
-        printf("salary=%.2f,tax=%.2f,sal after tax=%.2f",salary,tax,salary-tax);
-    }
+        printf("please enter work time:\n");
+        scanf("%lf",&time);
+        switch (flag)
+        {
+            case 1:showsal(SAL1,time);break;
+            case 2:showsal(SAL2,time);break;
+            case 3:showsal(SAL3,time);break;
+            case 4:showsal(SAL4,time);break;
+            default:printf("Enter the right number.\n");break;
+        }
+        printf("**********************************************************************\n");
+        printf("Enter the number corresponding to the desired pay rate or action:\n");
+        printf("1) $8.75/hr\t\t\t2) $9.33/hr\n");
+        printf("3) $10.00/hr\t\t\t4) $11.20/hr\n");
+        printf("5) quit\n");
+        printf("**********************************************************************\n");
+        scanf("%d",&flag);
 
+    }
+    printf("Done.\n");
     return 0;
-
 }
+
+void showsal(double sal,double wtime)
+{
+    double allSalary,tax,netSalary;
+    if(wtime>BASETIME) wtime=BASETIME+(wtime-BASETIME)*1.5;
+    allSalary=wtime*sal;
+    if(allSalary<=RATEBASE1)
+    {
+        tax=allSalary*RATE1;
+        netSalary=allSalary-tax;
+        printf("allSalary=%.2lf,tax=%.2lf,netSalary=%.2lf.\n",allSalary,tax,netSalary);
+    }
+    if(allSalary>RATEBASE1&&allSalary<=RATEBASE2)
+    {
+        tax=RATEBASE1*RATE1+(allSalary-RATEBASE1)*RATE2;
+        netSalary=allSalary-tax;
+        printf("allSalary=%.2lf,tax=%.2lf,netSalary=%.2lf.\n",allSalary,tax,netSalary);
+    }
+    if(allSalary>RATEBASE2)
+    {
+        tax=RATEBASE1*RATE1+(RATEBASE2-RATEBASE1)*RATE2+(allSalary-RATEBASE2)*RATE3;
+        netSalary=allSalary-tax;
+        printf("allSalary=%.2lf,tax=%.2lf,netSalary=%.2lf.\n",allSalary,tax,netSalary);
+    }
+}
+
+
+
+
+
+
+
+
