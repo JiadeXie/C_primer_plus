4bb402c9799a639193027299d0f549299631875a
jiadexie <837578259@qq.com>
2022-11-03 16:25:27 +0800

    break用法

--- main.c
+++ main.c
@@ -2,36 +2,18 @@
 #include <stdio.h>
 int main(void)
 {
-    const float MIN=0.0f;
-    const float MAX=100.0f;
-
-    float score;
-    float total=0.0f;
-    int n=0;
-    float min=MAX;
-    float max=MIN;
-
-    printf("Enter the first score(q to quit): ");
-    while (scanf("%f,&score")==1)
+    float length,width;
+    printf("Enter the length of the rectangle:\n");
+    while (scanf("%f",&length)==1)
     {
-        if(score<MIN||score>MAX)
-        {
-            printf("%0.1f is an invalid value.Try again:",score);
-            continue;
-        }
-        printf("Accepting %0.1f:\n",score);
-        min=(score<min)?score:min;
-        max=(score>max)?score:max;
-        total+=score;
-        n++;
-        printf("Enter next score（q to quit): ");
+        printf("Length=%0.2f:\n",length);
+        printf("Enter its width:\n");
+        if(scanf("%f",&width)!=1) break;
+        printf("Width=%0.2f:\n",width);
+        printf("Are=%0.2f:\n",length*width);
+        printf("Enter the length of the rectangle:\n");
     }
-    if(n>0)
-    {
-        printf("Average of %d score is %0.1f.\n",n,total/n);
-        printf("Low=%0.1f,high=%0.1f\n",min,max);
-    }
-    else printf("No valid scores were entered.\n");
+    printf("Done.\n");
 
     return 0;
 
