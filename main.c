4e0ebb4e316fdf483ca0fd33f29532f523f02ca4
jiadexie <837578259@qq.com>
2022-11-07 02:16:23 +0800

    尾递归

--- main.c
+++ main.c
@@ -1,19 +1,44 @@
 #include <stdio.h>
-void up_and_down(int);
+long fact(int n);
+long rfact(int n);
 
 int main(void)
 {
-    up_and_down(1);
+    int num;
+
+    printf("This program calculates factorials.\n");
+    printf("Enter a value in the range 0-12 (q to quit):\n");
+    while (scanf("%d",&num)==1)
+    {
+        if(num<0) printf("No negative number,please.\n");
+        else if(num>12) printf("Keep input under 13.\n");
+        else
+        {
+            printf("loop: %d factorial=%ld\n",num, fact(num));
+            printf("recursion: %d factorial=%ld\n",num, rfact(num));
+        }
+        printf("Enter a value in the range 0-12 (q to quit):\n");
+    }
+    printf("Bye.\n");
+
     return 0;
 }
-void up_and_down(int n)
+
+long fact(int n)
 {
-    printf("Level %d£ºn location %p\n",n,&n);
-    if(n<4) up_and_down(n+1);
-    printf("LEVEL %d: n location %p\n",n,&n);
+    long ans;
+
+    for(ans=1;n>1;n--) ans*=n;
+    return ans;
 }
 
-
+long rfact(int n)
+{
+    long ans;
+    if(n>0) ans=n* rfact(n-1);
+    else ans=1;
+    return ans;
+}
 
 
 
