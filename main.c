2e2d57c2e3ab1edc14bff97d41d6707f782cb99f
jiadexie <837578259@qq.com>
2022-11-06 22:54:30 +0800

    简单函数示例

--- main.c
+++ main.c
@@ -1,97 +1,38 @@
 #include <stdio.h>
-#define SAL1 8.75
-#define SAL2 9.33
-#define SAL3 10.00
-#define SAL4 11.20
-#define BASETIME 40.0
-#define RATEBASE1 300.0
-#define RATEBASE2 450.0
-#define RATE1 0.15
-#define RATE2 0.20
-#define RATE3 0.25
+#include <string.h>
+#define NAME "GIGATHINK,INC."
+#define ADDRESS "101 Megabuck Plaza"
+#define PLACE "Megapolis, CA 94904"
+#define WIDTH 40
+#define SPACE ' '
+
+void show_n_char(char ch,int num);
 
 int main(void)
 {
-    void showsal(double,double);
-    int flag;
-    double time;
-    printf("**********************************************************************\n");
-    printf("Enter the number corresponding to the desired pay rate or action:\n");
-    printf("a) $8.75/hr\t\t\tb) $9.33/hr\n");
-    printf("c) $10.00/hr\t\t\td) $11.20/hr\n");
-    printf("q) quit\n");
-    printf("**********************************************************************\n");
-    scanf("%c",&flag);
-    while (getchar()!='\n') continue;
-    while (!(flag=='q'))
-    {
+    int spaces;
 
-        switch (flag)
-        {
-            case 'a':
-            {
-                printf("please enter work time:\n");
-                scanf("%lf",&time);
-                showsal(SAL1,time);break;
-            }
-            case 'b':
-            {
-                printf("please enter work time:\n");
-                scanf("%lf",&time);
-                showsal(SAL2,time);break;
-            }
-            case 'c':
-            {
-                printf("please enter work time:\n");
-                scanf("%lf",&time);
-                showsal(SAL3,time);break;
-            }
-            case 'd':
-            {
-                printf("please enter work time:\n");
-                scanf("%lf",&time);
-                showsal(SAL4,time);break;
-            }
-            default:printf("Enter the right letter.\n");break;
-        }
-        printf("**********************************************************************\n");
-        printf("Enter the number corresponding to the desired pay rate or action:\n");
-        printf("a) $8.75/hr\t\t\tb) $9.33/hr\n");
-        printf("c) $10.00/hr\t\t\td) $11.20/hr\n");
-        printf("q) quit\n");
-        printf("**********************************************************************\n");
-        scanf("%c",&flag);
-        while (getchar()!='\n') continue;
-    }
-    printf("Done.\n");
+    show_n_char('*',WIDTH);
+    putchar('\n');
+    show_n_char(SPACE,12);
+    printf("%s\n",NAME);
+    spaces=(WIDTH-strlen(ADDRESS))/2;
+
+    show_n_char(SPACE,spaces);
+    printf("%s\n",ADDRESS);
+    show_n_char(SPACE,(WIDTH-strlen(PLACE))/2);
+
+    printf("%s\n",PLACE);
+    show_n_char('*',WIDTH);
+    putchar('\n');
+    getchar();
     return 0;
 }
-
-void showsal(double sal,double wtime)
+void show_n_char(char ch,int num)
 {
-    double allSalary,tax,netSalary;
-    if(wtime>BASETIME) wtime=BASETIME+(wtime-BASETIME)*1.5;
-    allSalary=wtime*sal;
-    if(allSalary<=RATEBASE1)
-    {
-        tax=allSalary*RATE1;
-        netSalary=allSalary-tax;
-        printf("allSalary=%.2lf,tax=%.2lf,netSalary=%.2lf.\n",allSalary,tax,netSalary);
-    }
-    if(allSalary>RATEBASE1&&allSalary<=RATEBASE2)
-    {
-        tax=RATEBASE1*RATE1+(allSalary-RATEBASE1)*RATE2;
-        netSalary=allSalary-tax;
-        printf("allSalary=%.2lf,tax=%.2lf,netSalary=%.2lf.\n",allSalary,tax,netSalary);
-    }
-    if(allSalary>RATEBASE2)
-    {
-        tax=RATEBASE1*RATE1+(RATEBASE2-RATEBASE1)*RATE2+(allSalary-RATEBASE2)*RATE3;
-        netSalary=allSalary-tax;
-        printf("allSalary=%.2lf,tax=%.2lf,netSalary=%.2lf.\n",allSalary,tax,netSalary);
-    }
+    int count;
+    for(count=1;count<=num;count++) putchar(ch);
 }
-
 
 
 
