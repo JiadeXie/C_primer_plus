aff83859fa7587dc81cf921cf8a3ea58666d9329
jiadexie <837578259@qq.com>
2022-11-11 23:41:13 +0800

    指针数组与字符串数组

--- main.c
+++ main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
-#define MSG "I'm special"
+#define SLEN 40
+#define LIM 5
 
 int main(void)
 {
-    char ar[]=MSG;
-    const char* pt=MSG;
-    printf("address of \"I'm special\": %p \n","I'm special");
-    printf("            address ar: %p\n",ar);
-    printf("            address pt: %p\n",pt);
-    printf("            address of MSG: %p\n",MSG);
-    printf("address of \"I'm special\": %p \n","I'm special");
+    const char* mytalents[LIM]={
+            "Adding numbers swiftly",
+            "Multiplying accurately","Stashing data",
+            "Following instructions to the letter",
+            "Understanding the C language"
+    };
+    char yourtalents[LIM][SLEN]={
+            "Walking in a straight line",
+            "Sleeping","Watching television",
+            "Mailing letters","Reading email"
+    };
+    int i;
 
+    puts("Let's compare talents.");
+    printf("%-36s %-25s\n","My Talents","Your Talents");
+    for (i=0;i<LIM;i++) printf("%-36s %-25s\n",mytalents[i],yourtalents[i]);
+    printf("\nsizeof mytalents: %zd, sizeof yourtalents: %zd\n", sizeof(mytalents), sizeof(yourtalents));
 
 
     return 0;
-
 }
