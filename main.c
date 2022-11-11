3bba54aaedbe4c41e6fa3df313a477cfc9bbdcc3
jiadexie <837578259@qq.com>
2022-11-12 01:09:02 +0800

    get()函数示例

--- main.c
+++ main.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
-#define SLEN 40
-#define LIM 5
+#define STLEN 81
 
 int main(void)
 {
-    const char* mytalents[LIM]={
-            "Adding numbers swiftly",
-            "Multiplying accurately","Stashing data",
-            "Following instructions to the letter",
-            "Understanding the C language"
-    };
-    char yourtalents[LIM][SLEN]={
-            "Walking in a straight line",
-            "Sleeping","Watching television",
-            "Mailing letters","Reading email"
-    };
-    int i;
+    char words[STLEN];
 
-    puts("Let's compare talents.");
-    printf("%-36s %-25s\n","My Talents","Your Talents");
-    for (i=0;i<LIM;i++) printf("%-36s %-25s\n",mytalents[i],yourtalents[i]);
-    printf("\nsizeof mytalents: %zd, sizeof yourtalents: %zd\n", sizeof(mytalents), sizeof(yourtalents));
-
+    puts("Enter a string,please.");
+    gets(words);
+    printf("Your string twice:\n");
+    printf("%s\n",words);
+    puts(words);
+    puts("Done.");
 
     return 0;
 }
