8e150c3caa3205d24b7bbc36ae66565e78d93ebe
jiadexie <837578259@qq.com>
2022-11-12 06:27:38 +0800

    strcmp()示例2

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-#define SIZE 40
-#define ANSWER "Grant"
+#define SIZE 80
+#define LIM 10
+#define STOP "quit"
 char* s_gets(char* st,int n);
 
 int main(void)
 {
-    char try[SIZE];
-    puts("Who is buried in Grant's tomb?");
-    s_gets(try,SIZE);
-    while (strcmp(try,ANSWER)!=0)
+    char input[LIM][SIZE];
+    int ct=0;
+
+    printf("Enter up to %d lines (type quit to quit):\n",LIM);
+    while (ct<LIM&& s_gets(input[ct],SIZE)!=NULL&& strcmp(input[ct],STOP)!=0)
     {
-        puts("No,that's wrong.Try again.");
-        s_gets(try,SIZE);
+        ct++;
     }
-    puts("That's right!");
+    printf("%d strings entered\n",ct);
 
     return 0;
 }
