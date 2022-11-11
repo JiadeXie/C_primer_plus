5161c0d151c4c3869772e1c0a5c9dd8d3bca3606
jiadexie <837578259@qq.com>
2022-11-12 02:41:05 +0800

    fgets()的特殊用法

--- main.c
+++ main.c
@@ -4,10 +4,18 @@
 int main(void)
 {
     char words[STLEN];
+    int i;
 
     puts("Enter strings (empty line to quit):");
     while (fgets(words,STLEN,stdin)!=NULL && words[0]!='\n')
-        fputs(words,stdout);
+    {
+        i=0;
+        while (words[i]!='\n'&&words[i]!='\0') i++;
+        if(words[i]=='\n')  words[i]='\0';//当输入的字符串较短时
+        else//当输入的字符串超出数组长度时
+            while (getchar()!='\n') continue;
+        puts(words);
+    }
     puts("Done.");
 
     return 0;
