28b7755a707af40d557a84ad603f9cd1a0f51587
jiadexie <837578259@qq.com>
2022-11-12 02:21:10 +0800

    fgets()与fputs()函数

--- main.c
+++ main.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
-#define STLEN 14
+#define STLEN 10
 
 int main(void)
 {
     char words[STLEN];
 
-    puts("Enter a string,please.");
-    fgets(words,STLEN,stdin);
-    printf("Your string twice (puts(),then fputs()):\n");
-    puts(words);
-    fputs(words,stdout);
-    puts("Enter another string,please.");
-    fgets(words,STLEN,stdin);
-    printf("Your string twice (puts(), then fputs()):\n");
-    puts(words);
-    fputs(words,stdout);
+    puts("Enter strings (empty line to quit):");
+    while (fgets(words,STLEN,stdin)!=NULL && words[0]!='\n')
+        fputs(words,stdout);
     puts("Done.");
 
     return 0;
