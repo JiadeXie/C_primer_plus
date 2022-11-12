25e19cec45f7451f7d56d0a136114a24d32fbb8f
jiadexie <837578259@qq.com>
2022-11-12 22:02:33 +0800

    strncpy()函数示例

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <string.h>
 #define SIZE 40
 #define LIM 5
+#define TARGSIZE 7
 char* s_gets(char* st,int n);
 
 int main(void)
 {
-    char qwords[LIM][SIZE];
+    char qwords[LIM][TARGSIZE];
     char temp[SIZE];
     int i=0;
 
@@ -16,7 +17,8 @@ int main(void)
         if(temp[0]!='q') printf("%s doesn't begin with q!\n",temp);
         else
         {
-            strcpy(qwords[i],temp);
+            strncpy(qwords[i],temp,TARGSIZE-1);
+            qwords[i][TARGSIZE-1]='\0';
             i++;
         }
     }
