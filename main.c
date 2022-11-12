64486b1ff96b0ab3a21d4163c731ff9c237fd7f6
jiadexie <837578259@qq.com>
2022-11-13 03:09:46 +0800

    修改字符串

--- main.c
+++ main.c
@@ -1,60 +1,42 @@
 #include <stdio.h>
 #include <string.h>
-#define SIZE 81
-#define LIM 20
-#define HALT ""
-void stsrt(char* strings[],int num);
-char* s_gets(char* st,int n);
+#include <ctype.h>
+#define LIMIT 81
+void ToUpper(char*);
+int PunctCount(const char*);
 
 int main(void)
 {
-    char input[LIM][SIZE];
-    char *ptstr[LIM];
-    int ct=0;
-    int k;
-    printf("Input up to %d lines,and I will sort them.\n",LIM);
-    printf("To stop,press the Enter key at a line's start.\n");
-    while (ct<LIM&& s_gets(input[ct],SIZE)!=NULL&&input[ct][0]!='\0')
-    {
-        ptstr[ct]=input[ct];
-        ct++;
-    }
-    stsrt(ptstr,ct);
-    puts("\nHere's the sorted list:\n");
-    for (k=0;k<ct;k++) puts(ptstr[k]);
+    char line[LIMIT];
+    char* find;
+
+    puts("Please enter a line:");
+    fgets(line,LIMIT,stdin);
+    find= strchr(line,'\n');
+    if(find) *find='\0';
+    ToUpper(line);
+    puts(line);
+    printf("That line has %d punctuation characters.\n", PunctCount(line));
 
     return 0;
 }
 
-void stsrt(char* strings[],int num)
+void ToUpper(char* str)
 {
-    char *temp;
-    int top,seek;
-
-    for(top=0;top<num-1;top++)
-        for (seek=top+1;seek<num;seek++)
-            if(strcmp(strings[top],strings[seek])>0)
-            {
-                temp=strings[top];
-                strings[top]=strings[seek];
-                strings[seek]=temp;
-            }
-}
-
-char* s_gets(char* st,int n)
-{
-    char* ret_val;
-    int i=0;
-
-    ret_val= fgets(st,n,stdin);
-    if(ret_val)
+    while (*str)
     {
-        while (st[i]!='\n'&&st[i]!='\0') i++;
-        if(st[i]=='\n') st[i]='\0';
-        else while (getchar()!='\n') continue;
+        *str= toupper(*str);
+        str++;
     }
-
-    return ret_val;
 }
 
-
+int PunctCount(const char* str)
+{
+    int ct=0;
+    while (*str)
+    {
+        if(ispunct(*str)) ct++;
+        str++;
+    }
+    return ct;
+}
