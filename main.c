6c87c8f9f6d3be8e4285839f565273e5ef0bb3af
jiadexie <837578259@qq.com>
2022-11-15 04:15:25 +0800

    掷骰子

--- main.c
+++ main.c
@@ -1,80 +1,40 @@
 #include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-char* s_gets(char* st,int n);
-void input(char *);
+#include <stdlib.h>
+#include <time.h>
+#include "diceroll.h"
 
 int main(void)
 {
-    char ch;
-    int inword=0;
-    int words=0,ul=0,ll=0,nc=0,pc=0;
-    while ((ch=getchar())!=EOF)
-    {
-        if(isalpha(ch))
-        {
-            if(inword==0)
-            {
-                words++;
-                inword=1;
-            };
+    int dice,roll;
+    int sides;
+    int status;
 
-            if(isupper(ch)) ul++;
-            if(islower(ch)) ll++;
-        }
-        if(isdigit(ch))
+    srand((unsigned int) time(0));
+    printf("Enter the number of sides per die,0 to stop.\n");
+    while (scanf("%d",&sides)==1&&sides>0)
+    {
+        printf("How many dice?\n");
+        if((status= scanf("%d",&dice))!=1)
         {
-            nc++;
-            inword=0;
+            if(status==EOF) break;
+            else
+            {
+                printf("You should have entered an integer.");
+                printf("Let's begin again.\n");
+                while (getchar()!='\n') continue;
+                printf("How many sides? Enter 0 to stop.\n");
+                continue;
+            }
         }
-        if(!isalnum(ch))
-        {
-            inword=0;
-            if(ch!=' '&&ch!='\t'&&ch!='\n') pc++;
-        }
+        roll=roll_n_dice(dice,sides);
+        printf("You have rolled a %d using %d %d-sided dice.\n",
+               roll,dice,sides);
+        printf("How many sides? Enter 0 to stop.\n");
     }
-    printf("words=%d,ul=%d,ll=%d,nc=%d,pc=%d",words,ul,ll,nc,pc);
+    printf("The rollem() function was called %d times.\n",roll_count);
+    printf("GOOD FORTUNE TO YOU!\n");
+
     getchar();
     return 0;
 }
 
-char* s_gets(char* st,int n)
-{
-    char* ret_val;
-    int i=0;
-
-    ret_val= fgets(st,n,stdin);
-    if(ret_val)
-    {
-        while (st[i]!='\n'&&st[i]!='\0') i++;
-        if(st[i]=='\n')  st[i]='\0';
-        else while (getchar()!='\n') continue;
-    }
-
-    return ret_val;
-}
-
-void input(char *str)
-{
-    char s[101];
-    char *pt=str;
-    int i=0;
-    do {
-        printf("please input a string(input ¿Õ¸ñ in a new line to quit):\n");
-        fgets(str,101,stdin);
-        if(str[0]=='\n') break;
-        while (*str!='\0')
-        {
-            if(*str!=' ')
-            {
-                s[i++]=*str;
-                str++;
-            }
-            else str++;
-        }
-        s[i]='\0';
-        strcpy(pt,s);
-        puts(pt);
-
-    }while(1);
-}
