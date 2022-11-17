b8b11aef12d63485efac64af8710ab39093416b8
jiadexie <837578259@qq.com>
2022-11-17 21:00:50 +0800

    用二进制IO进行随机访问

--- main.c
+++ main.c
@@ -1,84 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#define BUFSIZE 4096
-#define SLEN 81
-void append(FILE* source,FILE* dest);
-char* s_gets(char* st,int n);
+#define ARSIZE 1000
 
 int main(void)
 {
-    FILE *fa,*fs;
-    int files=0;
-    char file_app[SLEN];
-    char file_src[SLEN];
-    int ch;
+    double numbers[ARSIZE];
+    double value;
+    const char* file="numbers.dat";
+    int i;
+    long pos;
+    FILE *iofile;
 
-    puts("Enter name of destination file:\n");
-    s_gets(file_app,SLEN);
-    if((fa= fopen(file_app,"a+"))==NULL)
+    for (i=0;i<ARSIZE;i++)  numbers[i]=100.0*i+1.0/(i+1);
+
+    if((iofile= fopen(file,"wb"))==NULL)
     {
-        fprintf(stderr,"Can't open %s\n",file_app);
+        fprintf(stderr,"Could not open %s for output.\n",file);
         exit(EXIT_FAILURE);
     }
-    if(setvbuf(fa,NULL,_IOFBF,BUFSIZE)!=0)
+    fwrite(numbers,sizeof (double),ARSIZE,iofile);
+    fclose(iofile);
+    if((iofile= fopen(file,"rb"))==NULL)
     {
-        fputs("Can't create output buffer\n",stderr);
+        fprintf(stderr,"Could not open %s for random access.\n",file);
         exit(EXIT_FAILURE);
     }
-    puts("Enter name of first source file (empty line to quit):");
-    while (s_gets(file_src,SLEN)&&file_src[0]!='\0')
+
+    printf("Enter an index in the range 0-%d.\n",ARSIZE-1);
+    while (scanf("%d",&i)==1&&i>=0&&i<ARSIZE)
     {
-        if(strcmp(file_src,file_app)==0) fputs("Can't append file to itself\n",stderr);
-        else if((fs= fopen(file_src,"r"))==NULL) fprintf(stderr,"Can't open %s\n",file_src);
-        else
-        {
-            if(setvbuf(fs,NULL,_IOFBF,BUFSIZE)!=0)
-            {
-                fputs("Can't create input buffer\n",stderr);
-                continue;
-            }
-            append(fs,fa);
-            if(ferror(fs)!=0) fprintf(stderr,"Error in reading file %s.\n",file_src);
-            if(ferror(fa)!=0) fprintf(stderr,"Error in writing file %s.\n",file_app);
-            fclose(fs);
-            files++;
-            printf("File %s appended.\n",file_src);
-            puts("Next file (empty line to quit):");
-        }
+        pos=(long)i*sizeof(double);
+        fseek(iofile,pos,SEEK_SET);
+        fread(&value,sizeof (double ),1,iofile);
+        printf("The value there is %f.\n",value);
+        printf("Next index (out of range to quit):\n");
     }
-    printf("Done appending. %d files appened.\n",files);
-    rewind(fa);
-    printf("%s contents:\n",file_app);
-    while ((ch= getc(fa))!=EOF) putchar(ch);
-    putchar('\n');
-    puts("Done displaying.\n");
-    fclose(fa);
+
+    fclose(iofile);
+    puts("Bye!");
 
     return 0;
-}
-
-void append(FILE *source,FILE *dest)
-{
-    size_t bytes;
-    static char temp[BUFSIZE];
-
-    while ((bytes= fread(temp,sizeof (char),BUFSIZE,source))>0)
-        fwrite(temp,sizeof (char),bytes,dest);
-}
-
-char* s_gets(char* st,int n)
-{
-    char* ret_val;
-    char* find;
-
-    ret_val= fgets(st,n,stdin);
-    if(ret_val)
-    {
-        find= strchr(st,'\n');
-        if(find) *find='\0';
-        else
-            while (getchar()!='\n') continue;
-    }
-    return ret_val;
 }
