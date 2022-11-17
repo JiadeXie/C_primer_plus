64d608f83c51a806aee026baff827a84f9fb650d
jiadexie <837578259@qq.com>
2022-11-17 13:24:29 +0800

    将文件附加到另一个文件末尾

--- main.c
+++ main.c
@@ -1,33 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define BUFSIZE 4096
 #define SLEN 81
-#define CNTL_Z '\032'
+void append(FILE* source,FILE* dest);
+char* s_gets(char* st,int n);
 
 int main(void)
 {
-    char file[SLEN];
-    char ch;
-    FILE *fp;
-    long count,last;
+    FILE *fa,*fs;
+    int files=0;
+    char file_app[SLEN];
+    char file_src[SLEN];
+    int ch;
 
-    puts("Enter the name of the file to be processed£º\n");
-    scanf("%80s",file);
-    if((fp=fopen(file,"rb"))==NULL)
+    puts("Enter name of destination file:\n");
+    s_gets(file_app,SLEN);
+    if((fa= fopen(file_app,"a+"))==NULL)
     {
-        printf("reverse can't open %s\n",file);
+        fprintf(stderr,"Can't open %s\n",file_app);
         exit(EXIT_FAILURE);
     }
-
-    fseek(fp,0L,SEEK_END);
-    last=ftell(fp);
-    for(count=1L;count<=last;count++)
+    if(setvbuf(fa,NULL,_IOFBF,BUFSIZE)!=0)
     {
-        fseek(fp,-count,SEEK_END);
-        ch=getc(fp);
-        if(ch!=CNTL_Z&&ch!='\r') putchar(ch);
+        fputs("Can't create output buffer\n",stderr);
+        exit(EXIT_FAILURE);
     }
+    puts("Enter name of first source file (empty line to quit):");
+    while (s_gets(file_src,SLEN)&&file_src[0]!='\0')
+    {
+        if(strcmp(file_src,file_app)==0) fputs("Can't append file to itself\n",stderr);
+        else if((fs= fopen(file_src,"r"))==NULL) fprintf(stderr,"Can't open %s\n",file_src);
+        else
+        {
+            if(setvbuf(fs,NULL,_IOFBF,BUFSIZE)!=0)
+            {
+                fputs("Can't create input buffer\n",stderr);
+                continue;
+            }
+            append(fs,fa);
+            if(ferror(fs)!=0) fprintf(stderr,"Error in reading file %s.\n",file_src);
+            if(ferror(fa)!=0) fprintf(stderr,"Error in writing file %s.\n",file_app);
+            fclose(fs);
+            files++;
+            printf("File %s appended.\n",file_src);
+            puts("Next file (empty line to quit):");
+        }
+    }
+    printf("Done appending. %d files appened.\n",files);
+    rewind(fa);
+    printf("%s contents:\n",file_app);
+    while ((ch= getc(fa))!=EOF) putchar(ch);
     putchar('\n');
-    fclose(fp);
+    puts("Done displaying.\n");
+    fclose(fa);
 
     return 0;
+}
+
+void append(FILE *source,FILE *dest)
+{
+    size_t bytes;
+    static char temp[BUFSIZE];
+
+    while ((bytes= fread(temp,sizeof (char),BUFSIZE,source))>0)
+        fwrite(temp,sizeof (char),bytes,dest);
+}
+
+char* s_gets(char* st,int n)
+{
+    char* ret_val;
+    char* find;
+
+    ret_val= fgets(st,n,stdin);
+    if(ret_val)
+    {
+        find= strchr(st,'\n');
+        if(find) *find='\0';
+        else
+            while (getchar()!='\n') continue;
+    }
+    return ret_val;
 }
