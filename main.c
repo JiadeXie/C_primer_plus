45dd7915485fc5e7cb0e21b4836d2d220c67c192
jiadexie <837578259@qq.com>
2022-11-18 15:22:16 +0800

    文件拷贝程序

--- main.c
+++ main.c
@@ -3,86 +3,47 @@
 #include <string.h>
 #define BUFSIZE 4096
 #define SLEN 81
-void append(FILE* source,FILE* dest);
-char* s_gets(char* st,int n);
+
 
 int main(int argc,char* argv[])
 {
-    FILE *fa,*fs;
-    int files;
-    int i=1;
-    //char file_app[SLEN];
-    //char file_src[SLEN];
-    int ch;
+    FILE *fs,*fd;
+    static char temp[BUFSIZE];
+    int bytes;
 
-    //puts("Enter name of destination file:\n");
-    //s_gets(file_app,SLEN);
-    if((fa= fopen(argv[1],"a+"))==NULL)
+    if(argc!=3)
     {
-        fprintf(stderr,"Can't open %s\n",argv[1]);
+        printf("argc error.\n");
         exit(EXIT_FAILURE);
     }
-    if(setvbuf(fa,NULL,_IOFBF,BUFSIZE)!=0)
+    if((fs= fopen(argv[1],"rb"))==NULL)
     {
-        fputs("Can't create output buffer\n",stderr);
+        printf("can not open %s file,\n ",argv[1]);
         exit(EXIT_FAILURE);
     }
-    files=argc-2;
-    //puts("Enter name of first source file (empty line to quit):");
-    //while (s_gets(file_src,SLEN)&&file_src[0]!='\0')
-    //{
-    while(i<=files)
+    if(setvbuf(fs,NULL,_IOFBF,BUFSIZE)!=0)
     {
-    if ((fs = fopen(argv[i+1], "r")) == NULL) fprintf(stderr, "Can't open %s\n", argv[i+1]);
-    else if (fs == fa) fputs("Can't append file to itself\n", stderr);
-    else {
-        if (setvbuf(fs, NULL, _IOFBF, BUFSIZE) != 0) {
-            fputs("Can't create input buffer\n", stderr);
-            continue;
-        }
-        append(fs, fa);
-        if (ferror(fs) != 0) fprintf(stderr, "Error in reading file %s.\n", argv[i+1]);
-        if (ferror(fa) != 0) fprintf(stderr, "Error in writing file %s.\n", argv[1]);
-        fclose(fs);
-        //files++;
-        printf("File %s appended.\n", argv[i+1]);
-        i++;
-        //puts("Next file (empty line to quit):");
-        }
+        printf("Can't create input buffer\n",stderr);
+        exit(EXIT_FAILURE);
     }
-    //}
-    printf("Done appending. %d files appened.\n",files);
-    rewind(fa);
-    printf("%s contents:\n",argv[1]);
-    while ((ch= getc(fa))!=EOF) putchar(ch);
+    if((fd= fopen(argv[2],"ab+"))==NULL)
+    {
+        printf("can not open %s file,\n ",argv[2]);
+        exit(EXIT_FAILURE);
+    }
+    if(setvbuf(fd,NULL,_IOFBF,BUFSIZE)!=0)
+    {
+        printf("Can't create output buffer\n",stderr);
+        exit(EXIT_FAILURE);
+    }
+    while ((bytes= fread(temp,sizeof (char),BUFSIZE,fs))>0)
+        fwrite(temp,sizeof (char),bytes,fd);
+    printf("Copy done.\n");
+    printf("Destination file content:\n");
+    rewind(fd);
+    while ((bytes= fread(temp,sizeof (char),BUFSIZE,fd))>0)
+        fputs(temp,stdout);
     putchar('\n');
-    puts("Done displaying.\n");
-    fclose(fa);
 
     return 0;
-}
-
-void append(FILE *source,FILE *dest)
-{
-    size_t bytes;
-    static char temp[BUFSIZE];
-
-    while ((bytes= fread(temp,sizeof (char),BUFSIZE,source))>0)
-        fwrite(temp,sizeof (char),bytes,dest);
-}
-
-char* s_gets(char* st,int n)
-{
-    char* ret_val;
-    char* find;
-
-    ret_val= fgets(st,n,stdin);
-    if(ret_val)
-    {
-        find= strchr(st,'\n');
-        if(find) *find='\0';
-        else
-            while (getchar()!='\n') continue;
-    }
-    return ret_val;
 }
