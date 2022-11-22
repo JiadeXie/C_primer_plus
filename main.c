e58b4f41e040e5023d5ae35009cbcbe95dd65e47
jiadexie <837578259@qq.com>
2022-11-23 03:25:51 +0800

    结构体数组特点元素的初始化

--- main.c
+++ main.c
@@ -5,42 +5,27 @@
 #define LEN 81
 char* s_gets(char* st,int n);
 
-typedef struct {
-    char name[10];
-    char shorname[4];
-    int days;
-    int number;
-}MONTH;
+typedef struct lens{
+    float foclen;
+    float fstop;
+    char brand[30];
+}LENS;
 
-MONTH year[12]={
-        {"January","Jan",31,1},
-        {"February","Feb",28,2},
-        {"March","Mar",31,3},
-        {"April","Apr",30,4},
-        {"May","May",31,5},
-        {"June","Jun",30,6},
-        {"July","Jul",31,7},
-        {"August","Aug",31,8},
-        {"September","Sep",30,9},
-        {"October","Oct",31,10},
-        {"November","Nov",30,11},
-        {"Decenber","Dec",31,12}
-};
+
 
 int main(void)
 {
-    int n;
-    int days;
-    printf("please enter a number(q to quit).\n");
-    while ((scanf("%d",&n))==1&&(n>=1&&n<=12))
-    {
-        for (int i=0;i<n;i++)
-        {
-            days+=year[i].days;
-        }
-        printf("there are %d days from 1 to %d month.\n",days,n);
-        printf("please enter a number(q to quit).\n");
+    LENS arr[10]={[3]={600.0,3.0,"Xeon"}};
+
+    arr[2].foclen=500.0;
+    arr[2].fstop=2.0;
+    strcpy(arr[2].brand,"Rema");
+
+    for (int i = 2; i <=3; ++i) {
+        printf("arr[%d].foclen=%f,arr[%d].fstop=%f,arr[%d].brand=%s.\n",
+               i,arr[i].foclen,i,arr[i].fstop,i,arr[i].brand);
     }
+
     puts("Bye!");
 
     getchar();
