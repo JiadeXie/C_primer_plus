ee5be69af8487114a5e4a46c41791ea83bd633d1
jiadexie <837578259@qq.com>
2022-11-26 11:02:22 +0800

    位字段和按位运算符

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <limits.h>
+/*位字段符号常量*/
 /*线的样式*/
 #define SOLID 0
 #define DOTTED 1
@@ -16,6 +17,22 @@
 #define CYAN (GREEN|BLUE)
 #define WHITE (RED|GREEN|BLUE)
 
+/*按位方法中用到的符合常量*/
+#define OPAQUE 0x1
+#define FILL_BLUE 0x8
+#define FILL_GREEN 0x4
+#define FILL_RED 0x2
+#define FILL_MASK 0xE
+#define BORDER 0x100
+#define BORDER_BLUE 0x800
+#define BORDER_GREEN 0x400
+#define BORDER_RED  0x200
+#define BORDER_MASK 0xE00
+#define B_SOLID 0
+#define B_DOTTED 0x1000
+#define B_DASHED 0x2000
+#define STYLE_MASK 0x3000
+
 const char* colors[8]={"black","red","green","yellow","blue","magenta","cyan","white"};
 
 struct box_props
@@ -29,19 +46,40 @@ struct box_props
     unsigned int :2;
 };
 
+union Views
+{
+    struct box_props st_view;
+    unsigned short us_view;
+};
+
 void show_settings(const struct box_props* pb);
+void show_settings1(unsigned short );
+char* itobs(int n,char* ps);
+
 
 int main(void)
 {
-    struct box_props box={true,YELLOW,true,GREEN,DASHED};
+    /*创建Views联合，并初始化initialize struct box view*/
+    union Views box={{true,YELLOW,true,GREEN,DASHED}};
+    char bin_str[8* sizeof(unsigned int)+1];
+
     printf("Original box settings:\n");
-    show_settings(&box);
-    box.opaque=false;
-    box.fill_color=WHITE;
-    box.border_color=MAGENTA;
-    box.border_style=SOLID;
-    printf("\nModified box settings:\n");
-    show_settings(&box);
+    show_settings(&box.st_view);
+    printf("\nBox settings using unsigned int view:\n");
+    show_settings1(box.us_view);
+    printf("bits are %s\n", itobs(box.us_view,bin_str));
+    box.us_view &=~FILL_MASK;
+    box.us_view |=(FILL_BLUE|FILL_GREEN);
+    box.us_view ^=OPAQUE;
+    box.us_view |=BORDER_RED;
+    box.us_view &=~STYLE_MASK;
+    box.us_view |=B_DOTTED;
+    printf("\nModfied box settings:\n");
+    show_settings(&box.st_view);
+    printf("\nBox settings using unsigned int view:\n");
+    show_settings1(box.us_view);
+    printf("bits are %s\n", itobs(box.us_view,bin_str));
+
 
     puts("Done!\n");
     getchar();
@@ -64,6 +102,33 @@ void show_settings(const struct box_props* pb)
         case DASHED:printf("dashed.\n");break;
         default:    printf("unknow type.\n");
     }
+}
+
+void show_settings1(unsigned short us)
+{
+    printf("Box is %s.\n",
+           (us&OPAQUE)==OPAQUE?"opaque":"transparent");
+    printf("The fill color is %s.\n",colors[(us>>1)&07]);
+    printf("Border %s.\n",(us&BORDER)==BORDER?"shown":"not shown");
+    printf("The border style is ");
+    switch (us&STYLE_MASK)
+    {
+        case B_SOLID :printf("solid.\n");break;
+        case B_DOTTED:printf("dotted.\n");break;
+        case B_DASHED:printf("dashed.\n");break;
+        default      :printf("unknow type.\n");
+    }
+    printf("The border color is %s.\n",colors[(us>>9)&07]);
+}
+
+char* itobs(int n,char* ps)
+{
+    int i;
+    const static int size=CHAR_BIT*sizeof(int);
+
+    for(i=size-1;i>=0;i--,n>>=1) ps[i]=(01&n)+'0';
+    ps[size]='\0';
+    return ps;
 }
 
 char* s_gets(char *st,int n)
