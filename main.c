45577c00da7c64936ea774937417a15087538878
jiadexie <837578259@qq.com>
2022-11-28 04:40:56 +0800

    形参数量可变的函数（stdarg.h)

--- main.c
+++ main.c
@@ -2,32 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#define SIZE 10
-void show_array(const int ar[],int n);
+#include <stdarg.h>
+double sum(int,...);
 
 int main(void)
 {
-    int values[SIZE]={1,2,3,4,5,6,7,8,9,10};
-    int target[SIZE];
-    double curious[SIZE/2]={2.0,2.0E5,2.0E10,2.0E20,5.0E30};
+    double s,t;
 
-    puts("memcpy() used:");
-    puts("values (original data): ");
-    show_array(values,SIZE);
-    memcpy(target,values,SIZE* sizeof(int));
-    puts("target (copy of values):");
-    show_array(target,SIZE);
-
-    puts("\nUsing memmove() with overlapping ranges:");
-    memmove(values+2,values,5* sizeof(int));
-    puts("values -- elements 0-4 copied to 2-6:");
-    show_array(values,SIZE);
-
-    puts("\nUsing memcpy() to copy double to int:");
-    memcpy(target,curious,(SIZE/2)* sizeof(double));
-    puts("target -- 5 doubles into 10 int positions:");
-    show_array(target,SIZE/2);
-    show_array(target+5,SIZE/2);
+    s= sum(3,1.1,2.5,13.3);
+    t= sum(6,1.1,2.1,13.1,4.1,5.1,6.1);
+    printf("return value for "
+           "sum(3,1.1,2.5,13.3):       %g\n",s);
+    printf("return value for "
+           "sum(6,1.1,2.1,13.1,4.1,5.1,6.1): %g\n",t);
 
     puts("Done!\n");
     getchar();
@@ -35,9 +22,15 @@ int main(void)
     return 0;
 }
 
-void show_array(const int ar[],int n)
+double sum(int lim,...)
 {
+    va_list ap;
+    double tot=0;
     int i;
-    for (i=0;i<n;i++) printf("%d ",ar[i]);
-    putchar('\n');
+
+    va_start(ap,lim);
+    for (i=0;i<lim;i++) tot+= va_arg(ap,double );
+    va_end(ap);
+
+    return tot;
 }
