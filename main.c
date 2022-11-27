70d59e9ba4e293e85af90671cd1361396e498fa1
jiadexie <837578259@qq.com>
2022-11-28 03:39:33 +0800

    memcpy()与memmove()示例

--- main.c
+++ main.c
@@ -2,22 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-
-#define NUM 40
-void fillarray(double ar[],int n);
-void showarray(const double ar[],int n);
-int mycomp(const void *p1,const void * p2);
+#define SIZE 10
+void show_array(const int ar[],int n);
 
 int main(void)
 {
-    double vals[NUM];
-    fillarray(vals,NUM);
-    puts("Random list:");
-    showarray(vals,NUM);
-    qsort(vals,NUM, sizeof(double),mycomp);
-    puts("\nSorted list:");
-    showarray(vals,NUM);
+    int values[SIZE]={1,2,3,4,5,6,7,8,9,10};
+    int target[SIZE];
+    double curious[SIZE/2]={2.0,2.0E5,2.0E10,2.0E20,5.0E30};
 
+    puts("memcpy() used:");
+    puts("values (original data): ");
+    show_array(values,SIZE);
+    memcpy(target,values,SIZE* sizeof(int));
+    puts("target (copy of values):");
+    show_array(target,SIZE);
+
+    puts("\nUsing memmove() with overlapping ranges:");
+    memmove(values+2,values,5* sizeof(int));
+    puts("values -- elements 0-4 copied to 2-6:");
+    show_array(values,SIZE);
+
+    puts("\nUsing memcpy() to copy double to int:");
+    memcpy(target,curious,(SIZE/2)* sizeof(double));
+    puts("target -- 5 doubles into 10 int positions:");
+    show_array(target,SIZE/2);
+    show_array(target+5,SIZE/2);
 
     puts("Done!\n");
     getchar();
@@ -25,31 +35,9 @@ int main(void)
     return 0;
 }
 
-void fillarray(double ar[],int n)
+void show_array(const int ar[],int n)
 {
-    int index;
-
-    for(index=0;index<n;index++)
-        ar[index]=(double)rand()/((double)rand()+0.1);
-}
-
-void showarray(const double ar[],int n)
-{
-    int index;
-
-    for (index=0;index<n;index++)
-    {
-        printf("%9.4f ",ar[index]);
-        if(index%6==5) putchar('\n');
-    }
-    if(index%6!=0) putchar('\n');
-}
-
-int mycomp(const void * p1,const void  * p2)
-{
-    const double * a1=(const double *)p1;
-    const double * a2=(const double *)p2;
-    if(*a1<*a2) return -1;
-    else if(*a1==*a2) return 0;
-    else return 1;
+    int i;
+    for (i=0;i<n;i++) printf("%d ",ar[i]);
+    putchar('\n');
 }
