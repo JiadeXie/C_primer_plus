3d32fac473de437f1799e54f54e326bc2ec7d031
jiadexie <837578259@qq.com>
2022-12-01 13:41:05 +0800

    电影.链表ver

--- main.c
+++ main.c
@@ -2,32 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 #define TSIZE 45
-#define FMAX 5
 
 struct film
 {
     char title[TSIZE];
     int rating;
+    struct film* next;
 };
 char* s_gets(char* st,int n);
 
 int main(void)
 {
-    struct film movies[FMAX];
-    int i=0;
-    int j;
-
-    puts("Enter first movie title:");
-    while (i<FMAX&& s_gets(movies[i].title,TSIZE)!=NULL&&movies[i].title[0]!='\0')
+    struct film *head=NULL;
+    struct film *prev,*current;
+    char input[TSIZE];
+    //收集并存储信息
+    puts("Enter first movie title:\n");
+    while (s_gets(input,TSIZE)!=NULL&&input[0]!='\0')
     {
+        current=(struct film*) malloc(sizeof(struct film));
+        if(head==NULL) head=current;
+        else prev->next=current;
+        current->next=NULL;
+        strcpy(current->title,input);
         puts("Enter your rating <0-10>:");
-        scanf("%d",&movies[i++].rating);
+        scanf("%d",&current->rating);
         while (getchar()!='\n') continue;
-        puts("Enter next movie title(empty line to quit):\n");
+        puts("Enter next movie title(empty line to stop):");
+        prev=current;
     }
-    if(i==0) printf("No data entered.");
+    //显示电影列表
+    if(head==NULL) printf("No data entered.");
     else printf("Here is the movie list:\n");
-    for (j=0;j<i;j++) printf("Movie: %s Rating: %d\n",movies[j].title,movies[j].rating);
+    current=head;
+    while (current!=NULL)
+    {
+        printf("Movie: %s Rating: %d\n",current->title,current->rating);
+        current=current->next;
+    }
+    //任务完成，释放内存
+    current=head;
+    while (current!=NULL)
+    {
+        head=current->next;
+        free(current);
+        current=head;
+    }
 
     puts("Done!\n");
     getchar();
