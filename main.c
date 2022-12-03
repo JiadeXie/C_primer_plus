734a034a42a4cf7d9fb73e210ab669670646c600
jiadexie <837578259@qq.com>
2022-12-04 02:42:09 +0800

    二叉树示例

--- main.c
+++ main.c
@@ -1,50 +1,139 @@
 #include <stdio.h>
-#include "queue.h"
+#include <string.h>
+#include <ctype.h>
+#include "tree.h"
+
+char menu(void);
+void addpet(Tree *pt);
+void droppet(Tree *pt);
+void showpets(const Tree* pt);
+void findpet(const Tree* pt);
+void printitem(Item item);
+void uppercase(char *str);
 char* s_gets(char* st,int n);
 
 int main(void)
 {
-    Queue line;
-    Item temp;
-    char ch;
+    Tree pets;
+    char choice;
 
-    InitializeQueue(&line);
-    puts("Testing the Queue interface.Type a to add a value,");
-    puts("type d to delete a value,and type q to quit.");
-    while ((ch= getchar())!='q')
+    InitializeTree(&pets);
+    while ((choice=menu())!='q')
     {
-        if(ch!='a'&&ch!='d') continue;
-        if(ch=='a')
+        switch (choice)
         {
-            printf("Integer to add:");
-            scanf("%d",&temp);
-            if(!QueueIsFull(&line))
-            {
-                printf("Putting %d into queue\n",temp);
-                EnQueue(temp,&line);
-            }
+            case 'a':addpet(&pets);break;
+            case 'l':showpets(&pets);break;
+            case 'f':findpet(&pets);break;
+            case 'n':printf("%d pets in club\n", TreeItemCount(&pets));break;
+            case 'd':droppet(&pets);break;
+            default:puts("Switching error");
         }
-        else
-            {
-                if(QueueIsEmpty(&line)) puts("Nothing to delete!");
-                else
-                {
-                    DeQueue(&temp,&line);
-                    printf("Removing %d from queue\n",temp);
-                }
-            }
-        printf("%d items in queue\n", QueueItemCount(&line));
-        puts("Type a to add,d to delete,q to quit:");
     }
+    DeleteAll(&pets);
 
-    EmptyTheQueue(&line);
     puts("Done!\n");
     getchar();
     getchar();
     return 0;
 }
 
+char menu(void)
+{
+    int ch;
 
+    puts("Nerfville Pet Club Membership Program");
+    puts("Enter the letter corresponding to your choice:");
+    puts("a) add a pet\t\tl)show list of pets");
+    puts("n) number of pets\tf) find pets");
+    puts("d) delete a pet\t\tq) quit");
+    while ((ch=getchar())!=EOF)
+    {
+        while (getchar()!='\n') continue;
+        ch= tolower(ch);
+        if(strchr("alrfndq",ch)==NULL) puts("Please enter an a,l,f,n,d or q:\n");
+        else break;
+    }
+    if(ch==EOF) ch='q';
+    return ch;
+}
+
+void addpet(Tree *pt)
+{
+    Item temp;
+    if(TreeIsFull(pt)) puts("No room in the club!\n");
+    else
+    {
+        puts("Please enter name of pet:");
+        s_gets(temp.petname,SLEN);
+        puts("Please enten pet kind:");
+        s_gets(temp.petkind,SLEN);
+        uppercase(temp.petname);
+        uppercase(temp.petkind);
+        AddItem(&temp,pt);
+    }
+}
+
+void showpets(const Tree *pt)
+{
+    if(TreeIsEmpty(pt)) puts("No entries!\n");
+    else Traverse(pt,printitem);
+}
+
+void printitem(Item item)
+{
+    printf("Pet: %-19s\nKind: %-19s\n",item.petname,item.petkind);
+}
+
+void findpet(const Tree *pt)
+{
+    Item temp;
+
+    if(TreeIsEmpty(pt))
+    {
+        puts("No entries!\n");
+        return;
+    }
+    puts("Please enter name of pet you wish to find:");
+    s_gets(temp.petname,SLEN);
+    puts("Please enter pet kind:");
+    s_gets(temp.petkind,SLEN);
+    uppercase(temp.petname);
+    uppercase(temp.petkind);
+    printf("%s the %s ",temp.petname,temp.petkind);
+    if(InTree(&temp,pt)) printf("is a member.\n");
+    else printf("is not a member.\n");
+}
+
+void droppet(Tree *pt)
+{
+    Item temp;
+
+    if(TreeIsEmpty(pt))
+    {
+        puts("No entries!\n");
+        return;
+    }
+
+    puts("Please enter name of pet you wish to delete:\n");
+    s_gets(temp.petname,SLEN);
+    puts("Please enter pet kind:");
+    s_gets(temp.petkind,SLEN);
+    uppercase(temp.petname);
+    uppercase(temp.petkind);
+    printf("%s the %s ",temp.petname,temp.petkind);
+    if(DeleteItem(&temp,pt)) printf("is dropped from the club.\n");
+    else printf("is not a member.\n");
+}
+
+void uppercase(char *str)
+{
+    while (*str)
+    {
+        *str= toupper(*str);
+        str++;
+    }
+}
 
 char* s_gets(char* st,int n)
 {
